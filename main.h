5c592ce7738be8b08d85d036f0ec69dfa37697a3
destinyOrji <destinyorji18@gmail.com>
2023-11-21 04:24:22 -0800

    login system- store imput into database.txt, check user input, create login which retuern success

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/*function prototypes*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(const char *s);
+void print_uns(unsigned int num, int *n);
+void print_oct(unsigned int list_num, int *n);
+
+#endif
